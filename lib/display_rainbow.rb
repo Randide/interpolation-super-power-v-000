586dffad36319b59c2b5db13e7bfee69b74a6463
Randi Nash <randidena@live.com>
2017-09-05 00:38:50 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
-colors ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-def display_rainbow
-  puts "colors[0]", "colors[1]", "colors[2]", "colors[3]", "colors[4]", "colors[5]", "colors[6]"
+def display_rainbow(unicorn)
+  puts "R: #{unicorn[0]}, O: #{unicorn[1]}, Y: #{unicorn[2]}, G: #{unicorn[3]}, B: #{unicorn[4]}, I: #{unicorn[5]}, V: #{unicorn[6]}"
 end
