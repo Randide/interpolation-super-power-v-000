7da11508e44dc999794f77aafae02416fbba81f8
Randi Nash <randidena@live.com>
2017-09-05 00:35:33 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,5 @@
 colors ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-def display_rainbow(colors)
+def display_rainbow
   puts "colors[0]", "colors[1]", "colors[2]", "colors[3]", "colors[4]", "colors[5]", "colors[6]"
 end
-
-  
